7eca7a2ff6a351b166718db23a35f1ea3f09d20d
DatBrian <vikard.2005@hotmail.com>
2023-06-28 01:10:38 -0500

    docs: :memo: Tipos de datos en php

--- php_basico.php
+++ php_basico.php
@@ -1,13 +1,22 @@
 <?php
 /*
-? Variables y constantes en php:
+? Tipos de datos en php:
+
+? En PHP, hay diferentes tipos de datos para almacenar información:
+
+    *-Enteros (int): Almacenan números enteros sin decimales.
+    *-Punto flotante (float): Almacenan números con decimales.
+    *-Booleanos (bool): Almacenan valores verdaderos o falsos.
+    *-Arreglos (array): Almacenan una colección de datos de diferentes tipos.
+    *-Cadenas de texto (string): Almacenan texto y caracteres.
+    *-Objetos (object): Almacenan instancias de clases, que son definiciones de objetos.
+    *-Recursos (resource): Almacenan referencias a recursos externos, como conexiones a bases de datos o archivos abiertos.
+    *-Nulos (null): Representan variables sin valor o sin definir.
+    *-Además de estos tipos básicos, PHP también admite constantes y tipos de datos compuestos, como estructuras de datos y clases.
+
+*Es esencial seleccionar el tipo de dato adecuado según la información que se desea almacenar y manipular, puedes usar la función especial var_dump() para imprimir el tipo de dato y el contenido de una variable.
+
+
 */
-//*En php, las variables se definen con un símbolo $ para indicar el nombre de la variable seguido del operador de asignación = y su valor, aqui un ejemplo de como declarar una variable en php:
-
-$nombre = "Brian";
-
-//*Las constantes en php como en otros lenguajes son espacios de memoria que no van a cambiar su valor, a contunuación un ejemplo de declaración de constantes en php:
-
-define("apellido", "Melo");
 
 ?>
