db9f21d8f74902ea53c040bbb3b63f8b8d30e0fc
DatBrian <vikard.2005@hotmail.com>
2023-06-28 01:28:03 -0500

    docs: :memo: Estructuras condicionales en php

--- php_basico.php
+++ php_basico.php
@@ -1,28 +1,46 @@
 <?php
 /*
-? isset() y empty():
+? Estructuras condicionales en php:
 
-? En PHP, las funciones isset() y empty() se utilizan para verificar el estado de una variable
+? En PHP, las estructuras de control son utilizadas para controlar el flujo de ejecución de un programa. Estas estructuras permiten tomar decisiones, repetir bloques de código y realizar acciones condicionales.
 */
 
-//* isset(): Esta función verifica si una variable está definida y tiene un valor asignado. Devuelve true si la variable existe y tiene un valor, y false si la variable no está definida o tiene un valor nulo como se muestra en el siguiente ejemplo:
+// ! Estructura if-else:
+//* La estructura if-else se utiliza para tomar decisiones basadas en una condición. Si la condición es verdadera, se ejecuta un bloque de código; de lo contrario, se ejecuta otro bloque de código como se muestra en el siguiente ejemplo:
 
-$nombre = "Juan";
+$edad = 20;
 
-if (isset($nombre)) {
-    echo "La variable \$nombre está definida y tiene un valor.";
+if ($edad >= 18) {
+    echo "Eres mayor de edad.";
 } else {
-    echo "La variable \$nombre no está definida o no tiene un valor.";
+    echo "Eres menor de edad.";
 }
 
-//* empty(): Esta función verifica si una variable está vacía. Devuelve true si la variable está vacía, es decir, si no tiene valor asignado, o si su valor se evalúa como falso (por ejemplo, una cadena vacía, el número 0 o el valor booleano false). Devuelve false si la variable tiene algún valor asignado y se evalúa como verdadero como se muestra en el siguiente ejemplo:
+//! Estructura switch-case
+//*La estructura switch-case se utiliza para realizar acciones diferentes según el valor de una expresión. Se compara la expresión con diferentes casos y se ejecuta el bloque de código correspondiente al caso que coincida como se muestra en el siguiente ejemplo:
 
-$edad = 0;
+$opcion = 2;
 
-if (empty($edad)) {
-    echo "La variable \$edad está vacía o se evalúa como falso.";
-} else {
-    echo "La variable \$edad tiene un valor asignado y se evalúa como verdadero.";
+switch ($opcion) {
+    case 1:
+        echo "Seleccionaste la opción 1.";
+        break;
+    case 2:
+        echo "Seleccionaste la opción 2.";
+        break;
+    case 3:
+        echo "Seleccionaste la opción 3.";
+        break;
+    default:
+        echo "Opción inválida.";
+        break;
+}
+
+//! Estructura for
+//*La estructura for se utiliza para repetir un bloque de código un número específico de veces. Se compone de una inicialización, una condición y una expresión de incremento como se muestra en el siguiente ejemplo:
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "El valor de i es: " . $i . "<br>";
 }
 
 
