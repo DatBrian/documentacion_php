94380b665f4e83f435df7d39d1da72deebf8bd97
DatBrian <vikard.2005@hotmail.com>
2023-06-28 00:53:40 -0500

    docs: :memo: Introducción a php

---
+++ php_basico.php
@@ -0,0 +1,13 @@
+/*
+? Introducción a php:
+
+*PHP es un lenguaje de programación de código abierto y del lado del servidor que se puede incrustar en documentos HTML. Es muy popular en el desarrollo web y ofrece una curva de aprendizaje rápida, lo que facilita la creación de aplicaciones orientadas al servidor.
+*Una de las ventajas de programar con PHP es su versatilidad, ya que puede implementarse en diferentes entornos de ejecución. Para desarrollar aplicaciones con PHP, se requiere contar con una herramienta que proporcione soporte para la ejecución a través de un servidor local.
+*Algunas de las herramientas comunes para configurar un entorno de desarrollo con PHP en Windows son:
+    -XAMPP
+    -WampServer
+    -Laragon
+*Cada herramienta tiene su propio proceso de instalación y configuración para configurar el servidor apache y mysql pero todas se utilizan para la configuración del entorno de desarrollo para PHP.
+*Una vez que hayas configurado el entorno de desarrollo con PHP en Windows o Linux, podrás comenzar a desarrollar aplicaciones utilizando PHP. PHP tiene una estructura básica de script, que incluye etiquetas de apertura y cierre del script. Puedes escribir la lógica y programación dentro del cuerpo del script.
+
+*/
